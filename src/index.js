cd507c09a95469e3cbf0669ecfb584b9e569c803
sahand12 <sahand.shok@gmail.com>
2018-07-12 16:38:39 +0430

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React form 'react';
+
+class HelloComponent extends React.Component {
+  render() {
+    return (
+      <div>hello there, this is my first react component</div>
+    );
+  }
+}
